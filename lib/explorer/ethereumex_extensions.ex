090b3e057c902a628a47f38b162c68e66d00f7b2
Derek Barnes and Matt Olenick <pair+dgb+matto@ministryofvelocity.com>
2018-02-20 14:37:02 -0800

    Create Ethereumex extensions to poll node for internal transactions

---
+++ lib/explorer/ethereumex_extensions.ex
@@ -0,0 +1,11 @@
+defmodule Explorer.EthereumexExtensions do
+  @moduledoc false
+
+  alias Ethereumex.HttpClient
+
+  def trace_transaction(hash) do
+    params = [hash, ["trace"]]
+    HttpClient.request("trace_replayTransaction", params, [])
+  end
+
+end
